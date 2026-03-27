dc1a30c693ffd7e09a33e26a9091fa7d104cf8c6
NUXX-G <nelsonffkarlsson@gmail.com>
2026-03-27 13:15:28 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/nuxx/savings_app/SavingsAppApplication.java
@@ -0,0 +1,13 @@
+package com.nuxx.savings_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsAppApplication.class, args);
+	}
+
+}
