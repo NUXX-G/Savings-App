5f62d4adc4f3b17e8f1210a827680b1aa612f269
NUXX-G <nelsonffkarlsson@gmail.com>
2026-03-27 14:00:24 +0100

    feat: add Usuario model

--- src/main/java/com/nuxx/savings_app/model/Usuario.java
+++ src/main/java/com/nuxx/savings_app/model/Usuario.java
@@ -1,5 +1,26 @@
 package com.nuxx.savings_app.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
 
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuario
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaRegistro;
 }
