79c7a2e37395d4615931aa48341abc250aa2fb26
NUXX-G <nelsonffkarlsson@gmail.com>
2026-03-30 22:24:41 +0200

    feat: add repositories for Usuario, Gasto, Ingreso and Meta

---
+++ src/main/java/com/nuxx/savings_app/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.nuxx.savings_app.repository;
+
+import com.nuxx.savings_app.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>
+{
+}
